38e1725793015ade7395b9eb8e2849e4fae147bc
Jeff Buttars <jeffbuttars@gmail.com>
2018-06-22 21:15:04 -0600

    Add search subscribe and fix some typos

--- service/handlers.py
+++ service/handlers.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 import datetime
 import random
 import uuid
@@ -11,6 +12,9 @@ from apistar.exceptions import WebSocketDisconnect
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+TOPICS = ('games', 'puzzles', 'vacations', 'programs', 'jobs', 'python', 'apistar')
 
 
 async def hello(ws: WebSocket):
@@ -26,7 +30,7 @@ async def ping_pong(ws: WebSocket):
 
     while True:
         try:
-            await ping = await ws.receive()
+            ping = await ws.receive()
 
             if ping != 'ping':
                 ws.close(code=status.WS_1002_PROT_ERROR)
@@ -45,7 +49,7 @@ async def consumer(ws: WebSocket):
 
     while True:
         try:
-            await data = await ws.receive()
+            data = await ws.receive()
 
             # Do something with the data
             # ....
@@ -61,7 +65,7 @@ async def consumer_of_json(ws: WebSocket):
 
     while True:
         try:
-            await data = await ws.receive_json()
+            data = await ws.receive_json()
 
             # Do something with the data, which will be parsed json
             # ....
@@ -116,6 +120,40 @@ async def timer(ws: WebSocket):
             return
 
 
+async def search_subscribe(topic: str, ws: WebSocket):
+    # Subscribe to a topic of information
+    if topic not in TOPICS:
+        # Make sure it's an approved topic
+        await ws.connect(close=True, close_code=status.WS_1007_INALID_DATA)
+        return
+
+    await ws.connect()
+
+    salt = ''
+
+    # Use a search result as our data source
+    async with aiohttp.ClientSession() as session:
+        while True:
+            url = 'http://api.duckduckgo.com/?q={}&format=json'.format(topic + salt)
+
+            async with session.get(url) as response:
+                response.raise_for_status()
+                result = await response.json()
+
+                try:
+                    # Get the results
+                    async for related_topic in result.get('RelatedTopics', []):
+                        await ws.send_json(related_topic)
+                        # Add some time in between sends to mimic an intermittent data source
+                        await asyncio.sleep(random.uniform(1.0, 5.0))
+                except WebSocketDisconnect:
+                    # Client is done, then so are we
+                    return
+
+            # Add some salt and query again
+            salt = ' ' + random.choice(TOPICS)
+
+
 routes = [
     Route('/', method='GET', handler=hello),
     Route('/ping_pong', method='GET', handler=ping_pong),
@@ -124,4 +162,5 @@ routes = [
     Route('/producer', method='GET', handler=producer),
     Route('/producer/of/json', method='GET', handler=producer_of_json),
     Route('/timer', method='GET', handler=timer),
+    Route('/search/subscribe', method='GET', handler=search_subscribe),
 ]
